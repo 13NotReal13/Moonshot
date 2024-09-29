7bc0d86c0e7bd16bfc01ebce9e858685c8ce6b06
Ivan Semikin <i.semikin.94@icloud.com>
2024-09-29 13:04:33 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Иван Семикин on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
